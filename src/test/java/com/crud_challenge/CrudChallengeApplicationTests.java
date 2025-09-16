7a19295fd9771145fd5f0a268ac7259223e2cff1
Eziiquiel <eziquielsilva64@gmail.com>
2025-09-16 09:56:47 -0300

    Configuring the database

---
+++ src/test/java/com/crud_challenge/CrudChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
